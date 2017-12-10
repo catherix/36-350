7bd550eb039d214259b404d272193ab10ec1a2dd
Catherine Xu <x.catherineh@gmail.com>
2017-12-10 17:49:29 -0500

    For Final Project Q20

---
+++ 350 Q20 Aliens Paragraph.R
@@ -0,0 +1,8 @@
+cat("Hello intelligent life,  if you're reading this, please let us (the humans) know! 
+    There are some things you should know about earth. 1. There are a lot of cool things here on earth, 
+    like coffee. 2. This is the first time we have made contact with extraterrestial
+    life so this is very exiciting for us! 
+    3. Here's everything NASA wanted to tell you, apparently: https://voyager.jpl.nasa.gov/golden-record/.
+    I have some questions for you. 1. How long have you known about us and this planet? 2. Have you talked
+    to other species from this planet? If so, which ones? 3. What is your planet like? 
+    That's all from me, I don't know how you found this message, but cool!")
